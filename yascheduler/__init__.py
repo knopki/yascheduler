413f61287b753af91d0aad7aae45037a05248c8f
Sergei Korolev <knopki@duck.com>
2026-05-22 17:21:02 +0300

    fix: add importlib.metadata fallback

--- yascheduler/__init__.py
+++ yascheduler/__init__.py
@@ -1,9 +1,13 @@
-import importlib.metadata
+from importlib.metadata import PackageNotFoundError, version
 
 from .client import Yascheduler
 from .variables import CONFIG_FILE, LOG_FILE, PID_FILE
 
-__version__ = importlib.metadata.version("yascheduler")
+try:
+    __version__ = version("yascheduler")
+except PackageNotFoundError:
+    __version__ = "0.1.0"
+
 __all__ = [
     "CONFIG_FILE",
     "LOG_FILE",
